cf9efbb9e6837cc6101cc4bcdf023a16a3ca6927
Tshepiso Eva Khoza"NOVA <evakhoza682@gmail.com>
2026-06-13 17:13:36 +0200

    commit and push

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -7,3 +8,15 @@ module.exports = {
   theme: { extend: {} },
   plugins: [],
 };
+=======
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  theme: { extend: {} },
+  plugins: [],
+};
+
+>>>>>>> 7095d4096b7b2926ba9bcac3e09b3b464217bbd1
